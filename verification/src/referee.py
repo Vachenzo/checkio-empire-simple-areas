85b4e99bd1c52309774ca08a420b27564a5c8b73
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-10 15:22:26 +0300

    Add Ranks

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,21 +1,23 @@
 from checkio_referee import RefereeBase
-from checkio_referee.covercode import py_unwrap_args
-from checkio_referee.validators import FloatEqualValidator
+from checkio_referee import covercodes
+from checkio_referee.util import validators
 
 import settings
 import settings_env
 from tests import TESTS
 
-FloatEqualValidator.PRECISION = 2
+Validator = validators.FloatEqualValidator
+
+Validator.PRECISION = 2
 
 class Referee(RefereeBase):
     TESTS = TESTS
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
     FUNCTION_NAME = "simple_areas"
-    VALIDATOR = FloatEqualValidator
+    VALIDATOR = Validator
     ENV_COVERCODE = {
-        "python_2": py_unwrap_args,
-        "python_3": py_unwrap_args,
+        "python_2": covercodes.py_unwrap_args,
+        "python_3": covercodes.py_unwrap_args,
         "javascript": None
     }
